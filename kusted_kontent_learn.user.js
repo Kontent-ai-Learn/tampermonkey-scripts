ea1168726bfb3ea04f2afbd08bdb331c6b469875
Tomas Nosek <tomasn@kentico.com>
2021-11-26 16:11:12 +0100

    (KK Learn) Add edit link and shortcut

--- kusted_kontent_learn.user.js
+++ kusted_kontent_learn.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         KustEd Kontent Docs improvements
 // @namespace    https://docs.kontent.ai/
-// @version      3.0.1
-// @description  Adds the preview link and keyboard shortcuts to the articles.
+// @version      3.1.0
+// @description  Adds the preview and edit links and keyboard shortcuts to the articles.
 // @author       Tomas Nosek, Kentico
 // @include      https://docs.kontent.ai/*
 // @icon         https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/custedlogo_48.png
@@ -17,13 +17,31 @@
     var $ = window.jQuery;
 
     const PREVIEW_ID = 'kusted_preview';
+    const EDIT_ID = 'kusted_edit';
 
     $(document).ready(async function() {
         await displayPreviewLink();
+        await displayEditLink();
     });
     
     async function displayPreviewLink() {
-        $('.article__notes').append('<a href="' + window.location.href.replace('docs.kontent.ai', 'kcd-web-preview-master.azurewebsites.net') + '" target="_blank" id="kusted_preview' + PREVIEW_ID + '" rel="noopener">Preview</a>');
+        $('.article__notes').append('<a href="' + window.location.href.replace('docs.kontent.ai', 'kcd-web-preview-master.azurewebsites.net') + '" target="_blank" id="' + PREVIEW_ID + '" rel="noopener">Preview</a>');
+    }
+
+    async function displayEditLink() {
+        var preview_link = $('#' + PREVIEW_ID).attr('href');
+        if (preview_link.length > 0) {
+            fetch(preview_link)
+                .then(response => response.text())
+                .then(text => {
+                var parser = new DOMParser();
+                var htmlDocument = parser.parseFromString(text, "text/html");
+                var edit_link = htmlDocument.documentElement.querySelector(".article__notes > a[rel='noopener']").href;
+                if (edit_link.length > 0) {
+                    $('.article__notes').append('<a href="' + edit_link + '" target="_blank" id="' + EDIT_ID + '" rel="noopener">Edit</a>');
+                }
+            });
+        }
     }
     
     $(document).keypress(function(e){
@@ -34,6 +52,11 @@
                 case 112: // key code for 'p'
                 case 119: // key code for 'w'
                     $('#' + PREVIEW_ID)[0].click();
+                    break;
+                case 101: // key code for 'e
+                    if ($('#' + EDIT_ID).length > 0) {
+                        $('#' + EDIT_ID)[0].click();
+                    }
             }
         }
     });
