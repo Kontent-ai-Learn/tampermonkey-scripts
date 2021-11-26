2f977ba65c67387c9ca0b5dbe9a66ecb7c1223b6
Tomas Nosek <tomasn@kentico.com>
2021-11-26 12:41:59 +0100

    [Jira board&backlog] Hide Insight button

--- kusted_jira_boards_backlogs.user.js
+++ kusted_jira_boards_backlogs.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         KustEd Jira board and backlog beautification
 // @namespace    https://docs.kontent.ai/
-// @version      3.0.3
+// @version      3.0.4
 // @description  Collapses the top bar for company-managed boards and backlogs
 // @author       Tomas Nosek, Kentico
 // @match        https://kentico.atlassian.net/jira/software/c/projects/
 // @include      https://kentico.atlassian.net/jira/software/c/projects/*/boards/*
-// @icon         https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/custedlogo_16.png
+// @icon         https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/custedlogo_32.png
 // @updateURL    https://github.com/KenticoDocs/tampermonkey-scripts/raw/master/kusted_jira_boards_backlogs.user.js
 // @resource     IMPORTED_CSS https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/kusted_jira_boards_backlogs.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
+// @commithandle Jira board&backlog
 // ==/UserScript==
 
 (function() {
