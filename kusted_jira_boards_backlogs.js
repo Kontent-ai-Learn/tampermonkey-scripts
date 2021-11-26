5c274fce7133a8e177a8a644d14e3b75451d9afd
Tomas Nosek <tomasn@kentico.com>
2021-11-26 12:12:27 +0100

    Intro commit with Jira beautification
    KustEd Jira board and backlog beautification 3.0

---
+++ kusted_jira_boards_backlogs.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         KustEd Jira board and backlog beautification
+// @namespace    https://docs.kontent.ai/
+// @version      3.0
+// @description  Collapses the top bar for company-managed boards and backlogs
+// @author       Tomas Nosek, Kentico
+// @match        https://kentico.atlassian.net/jira/software/c/projects/
+// @include      */boards/*
+// @icon         https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/custedlogo_16.png
+// @resource     IMPORTED_CSS https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/kusted_jira_boards_backlogs.css
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const css = GM_getResourceText("IMPORTED_CSS");
+    GM_addStyle(css);
+})();
