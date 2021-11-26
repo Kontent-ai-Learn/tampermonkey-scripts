79ba52246e4fefc3dc48749c60d3c7e643f5f4a1
Tomas Nosek <tomasn@kentico.com>
2021-11-26 13:02:24 +0100

    (Jira board&backlog) Replace script icon

--- kusted_jira_boards_backlogs.user.js
+++ kusted_jira_boards_backlogs.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         KustEd Jira board and backlog beautification
 // @namespace    https://docs.kontent.ai/
-// @version      3.0.4
-// @description  Collapses the top bar for company-managed boards and backlogs
+// @version      3.0.5
+// @description  Collapses the top bar for company-managed boards and backlogs.
 // @author       Tomas Nosek, Kentico
 // @match        https://kentico.atlassian.net/jira/software/c/projects/
 // @include      https://kentico.atlassian.net/jira/software/c/projects/*/boards/*
-// @icon         https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/custedlogo_32.png
+// @icon         https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/custedlogo_48.png
 // @updateURL    https://github.com/KenticoDocs/tampermonkey-scripts/raw/master/kusted_jira_boards_backlogs.user.js
 // @resource     IMPORTED_CSS https://raw.githubusercontent.com/KenticoDocs/tampermonkey-scripts/master/kusted_jira_boards_backlogs.css
 // @grant        GM_getResourceText
