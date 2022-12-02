f13af37a440b5e93be51f622fc056b265e44ac4e
Tomas Nosek <tom.nosek@gmail.com>
2022-12-02 08:21:24 +0100

    (Jira board&backlog) Fix alternate background color

--- kusted_jira_boards_backlogs.user.js
+++ kusted_jira_boards_backlogs.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         KustEd Jira board and backlog beautification
 // @namespace    https://kontent.ai/learn
-// @version      3.2.0
+// @version      3.2.1
 // @description  Collapses the top bar for company-managed boards and backlogs.
 // @author       Tomas Nosek, Kontent.ai
-// @match        https://kontent-ai.atlassian.net/jira/software/c/projects/
+// @match        https://kontent-ai.atlassian.net/jira/software/c/projects/*
 // @icon         https://raw.githubusercontent.com/Kontent-ai-Learn/tampermonkey-scripts/master/files/custedlogo_48.png
 // @updateURL    https://github.com/Kontent-ai-Learn/tampermonkey-scripts/raw/master/kusted_jira_boards_backlogs.user.js
 // @downloadURL  https://github.com/Kontent-ai-Learn/tampermonkey-scripts/raw/master/kusted_jira_boards_backlogs.user.js
